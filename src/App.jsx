f047c273334b6a9c4b979f8dc41dd22bfb55393c
Rupesh Dash <rupeshdash20@gmail.com>
2025-03-26 17:41:45 +0530

    17

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 
+// eslint-disable-next-line no-unused-vars
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./component/Navbar";
